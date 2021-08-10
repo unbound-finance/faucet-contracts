176b39de38fd9f21c1c60183d950583a7e26da05
ARNAB RAY <59610398+Arnab11917676@users.noreply.github.com>
2021-08-10 11:07:39 +0530

    Updated config to BSE

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,6 @@
-require('dotenv').config()
 require("@nomiclabs/hardhat-waffle");
-require("@nomiclabs/hardhat-etherscan");
+require('@nomiclabs/hardhat-ethers');
+const { mnemonic } = require('./secrest.json');
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -19,24 +19,41 @@ task("accounts", "Prints the list of accounts", async () => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.0",
-
+  defaultNetwork: "mainnet",
   networks: {
-    kovan: {
-      url: `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: [process.env.DEPLOYER_PRIVATE_KEY],
-      gas: 10000000,
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+    hardhat: {
+    },
+    testnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: {mnemonic: mnemonic}
     },
     mainnet: {
-      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: [process.env.DEPLOYER_PRIVATE_KEY],
-      gas: 10000000,
+      url: "https://bsc-dataseed.binance.org/",
+      chainId: 56,
+      gasPrice: 20000000000,
+      accounts: {mnemonic: mnemonic}
     }
   },
-
-  etherscan: {
-    // Your API key for Etherscan
-    // Obtain one at https://etherscan.io/
-    apiKey: "T9GV1IF4V7YDXQ8F53U1FK2KHCE2KUUD8Z"
+  solidity: {
+    version: "0.8.0",
+    settings: {
+      optimizer: {
+        enabled: true
+      }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 20000
   }
 };
